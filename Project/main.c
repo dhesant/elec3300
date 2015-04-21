416d53a454b406ae60150fe0a106b2dc7bdeb208
Dhesant Nakka <dhesant@gmail.com>
2015-04-21 20:38:46 +0800

    initial commit

---
+++ Project/main.c
@@ -0,0 +1,64 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+
+/* Private typedef -----------------------------------------------------------*/
+GPIO_InitTypeDef GPIO_InitStructure;
+EXTI_InitTypeDef EXTI_InitStructure;
+NVIC_InitTypeDef NVIC_InitStructure;
+uint32_t EXTI_Line;
+
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+
+int main(void)
+{  
+  /* Setup the microcontroller system. Initialize the Embedded Flash Interface,  
+     initialize the PLL and update the SystemFrequency variable. */
+  SystemInit();
+  
+  /* Configure all unused GPIO port pins in Analog Input mode (floating input
+     trigger OFF), this will reduce the power consumption and increase the device
+     immunity against EMI/EMC *************************************************/
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+                         RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD |
+                         RCC_APB2Periph_GPIOE |RCC_APB2Periph_GPIOF | 
+                         RCC_APB2Periph_GPIOG, ENABLE);  
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+  GPIO_Init(GPIOF, &GPIO_InitStructure);
+  GPIO_Init(GPIOG, &GPIO_InitStructure);  
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+                         RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD |
+                         RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF | 
+                         RCC_APB2Periph_GPIOG, DISABLE);  
+  
+  /* Global initialization ends above.*/
+  /* -------------------------------------------------------------------------- */
+
+  /* Please add your project initialization code below */
+
+
+
+  while (1)
+  {
+  /* Please add your project implementation code below */
+
+
+
+
+  }
+}
+
+
